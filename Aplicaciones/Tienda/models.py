db86198c5a881501624ce0e5190b650c8377e877
Franco <Francosk2017@gmail.com>
2025-09-24 17:55:22 -0300

    Agregar campo imagen y descripción a modelo Categoria con imágenes SVG personalizadas

--- Aplicaciones/Tienda/models.py
+++ Aplicaciones/Tienda/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 
 class Categoria(models.Model):
     nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+    imagen = models.ImageField(upload_to='categorias/', blank=True, null=True)
 
     def __str__(self):
         return self.nombre
